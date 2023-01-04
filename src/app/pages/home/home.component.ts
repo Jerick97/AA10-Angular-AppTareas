0effb50116db7024d5eb72bc287d9f7a721e6f97
Emerson Suarez <emersonsuarez2904@gmail.com>
2023-01-04 10:11:13 -0500

    Agregado método obtener productos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  titulo !: string;
+  descripcion !: string;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  obtenerTarea(title:string, description:string){
+    this.titulo = title;
+    this.descripcion = description;
+
+    alert(this.titulo + this.descripcion);
+  }
 }
